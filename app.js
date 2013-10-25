f0edab5b2734dba5f73fbe9126b9d5803eb6d317
shaunuk <shaun@pppoe.co.uk>
2013-10-25 19:59:37 +0000

    better as well, easier to read

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
+//declare required modules
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
+  , static = require('node-static')
   , sys = require('sys')
+  app.listen(8080);
 
-app.listen(8080);
-  var exec = require('child_process').exec;
+
+var file = new(static.Server)();
+
+function handler(request, response) {
+  console.log('serving file',request.url)
+  file.serve(request, response);
+};
+
+
+console.log('Pi Car we server listening on port 8080');
+
+
+var exec = require('child_process').exec;
+
+
+
+
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 
+/*
+//create a basic web server - port number specified above
 function handler (req, res) {
   fs.readFile(__dirname + '/socket.html',
   function (err, data) {
@@ -15,9 +35,12 @@ function handler (req, res) {
       return res.end('Error loading socket.html');
     }
     res.writeHead(200);
+    console.log('served up page');
     res.end(data);
   });
 }
+
+*/
 
 lastAction = "";
 
@@ -27,7 +50,7 @@ function emergencyStop(){
 
 io.sockets.on('connection', function (socket) {
   socket.on('fromclient', function (data) {
-  //console.log(data);
+  console.log(data);
   exec("echo 'sa "+data+"' > /dev/ttyAMA0", puts);
   clearInterval(lastAction);
   lastAction = setInterval(emergencyStop,1000);
