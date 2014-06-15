183d1585c92a5b134e20d9953811f23255d0008b
lawsonkeith <pi@raspberrypi.(none)>
2014-06-15 19:24:45 +0000

    initial commit

--- app.js
+++ app.js
@@ -4,30 +4,30 @@ var app = require('http').createServer(handler)
   , fs = require('fs')
   , static = require('node-static')
   , sys = require('sys')
-  , PwmDriver = require('adafruit-i2c-pwm-driver')
+  , piblaster = require('pi-blaster.js')
   , sleep = require('sleep')
   , argv = require('optimist').argv;
   app.listen(8080);
 
 
 //set the address and device name of the breakout board
-pwm = new PwmDriver(0x40,'/dev/i2c-0');
+//pwm = new PwmDriver(0x40,'/dev/i2c-0');
 
 //set pulse widths
-setServoPulse = function(channel, pulse) {
-  var pulseLength;
-  pulseLength = 1000000;
-  pulseLength /= 60;
-  print("%d us per period" % pulseLength);
-  pulseLength /= 4096;
-  print("%d us per bit" % pulseLength);
-  pulse *= 1000;
-  pulse /= pulseLength;
-  return pwm.setPWM(channel, 0, pulse);
-};
+//setServoPulse = function(channel, pulse) {
+//  var pulseLength;
+//  pulseLength = 1000000;
+//  pulseLength /= 60;
+//  print("%d us per period" % pulseLength);
+//  pulseLength /= 4096;
+//  print("%d us per bit" % pulseLength);
+//  pulse *= 1000;
+//  pulse /= pulseLength;
+//  return pwm.setPWM(channel, 0, pulse);
+//};
 
 //set pulse frequency
-pwm.setPWMFreq(60);
+//pwm.setPWMFreq(60);
 
 //Make a web server on port 8080
 
@@ -42,22 +42,24 @@ console.log('Pi Car we server listening on port 8080 visit http://ipaddress:8080
 lastAction = "";
 
 function emergencyStop(){
-  pwm.setPWM(0, 0, 400); //center front wheels
-  pwm.setPWM(1, 0, 330); //stop motor
+  //enter 0 point here specific to your pwm control
+  piblaster.setPwm(17, .14); //thr
+  piblaster.setPwm(18, .14); //spd
   console.log('###EMERGENCY STOP - signal lost or shutting down');
 }
 
 
 if (argv.beta) {
   console.log("\nPerforming one off servo position move to: "+argv.beta);
-  pwm.setPWM(0, 0, argv.beta); //using direct i2c pwm modue
-  pwm.stop();
+  //pwm.setPWM(0, 0, argv.beta); //using direct i2c pwm modue
+  //pwm.stop();
   return process.exit();
 }
 if (argv.gamma) {
+  //throttle
   console.log("\nPerforming one off servo position move to: "+argv.gamma);
-  pwm.setPWM(1, 0, argv.gamma); //using direct i2c pwm modue
-  pwm.stop();
+  //pwm.setPWM(1, 0, argv.gamma); //using direct i2c pwm modue
+  //pwm.stop();
   return process.exit();
 }
 
@@ -68,10 +70,10 @@ io.sockets.on('connection', function (socket) {
   console.log("Beta: "+data.beta+" Gamma: "+data.gamma);
   //exec("echo 'sa "+data+"' > /dev/ttyAMA0", puts); //using http://electronics.chroma.se/rpisb.php
   //exec("picar.py 0 "+data.beta, puts); //using python adafruit module
-  pwm.setPWM(0, 0, data.beta); //using direct i2c pwm modue
-  pwm.setPWM(1, 0, data.gamma); //using direct i2c pwm modue
+  piblaster.setPwm(18, data.beta); //steer using soft pwm
+  piblaster.setPwm(17, data.gamma); //throttle using soft pwm
   clearInterval(lastAction); //stop emergency stop timer
-  lastAction = setInterval(emergencyStop,1000); //set emergency stop timer for 1 second
+  lastAction = setInterval(emergencyStop,2000); //set emergency stop timer for 1 second
   });
 });
 
